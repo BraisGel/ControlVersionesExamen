e4184ab3c00e82451517b4ecb321d9b2ca7b0367
Brais <bgelabert19547@iesjoanramis.org>
2026-03-05 16:47:19 +0100

    Initial project setup

---
+++ Java/src/src/App.java
@@ -0,0 +1,21 @@
+package src;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Sistema iniciado");
+			double precioBase = 100;
+			double descuento = 10;
+			double iva = 21;
+			double precioFinal = calcularPrecioFinal(precioBase, descuento, iva);
+			mostrarResultado(precioFinal);
+	}
+	
+	public static double calcularPrecioFinal(double precio, double descuento, double iva) {
+		double precioConDescuento = precio - (precio * descuento / 100);
+		return precioConDescuento + (precioConDescuento * iva / 100);
+	}
+	
+	public static void mostrarResultado(double resultado) {
+		System.out.println("El precio final es: " + resultado);
+	}
+}
